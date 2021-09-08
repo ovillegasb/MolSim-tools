f909f97bba791ca0a24f6b020c3430303521313e
ovillegas <orlando.villegas@univ-pau.fr>
2021-09-08 17:17:48 +0200

    add module buildertopol.py, contains the constructor of the system topology

---
+++ pylmp/buildertopol.py
@@ -0,0 +1,11 @@
+"""
+Contendra las funciones necesarias para construir el objecto MOL con toda la informacion
+sobre la topologia molecular.
+
+"""
+
+
+class MOL:
+    """A class to represent a molecule and its properties."""
+
+    pass
